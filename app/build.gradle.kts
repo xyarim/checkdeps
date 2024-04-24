05828792b7ed931ceeb4641460445a4a85048dab
Arslan <arslan22011@gmail.com>
2024-04-24 14:47:05 +0500

    sda

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.xyarim.artifactstestdeps"
+        applicationId = "com.xyarim.testciapp"
         minSdk = 24
         targetSdk = 34
         versionCode = 3
