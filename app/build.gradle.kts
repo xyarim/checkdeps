f8866976ea47596c3c7417da18f5022fbda8e8a4
Arslan <arslan22011@gmail.com>
2024-04-24 15:01:34 +0500

    FIX

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.xyarim.testciapp"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
